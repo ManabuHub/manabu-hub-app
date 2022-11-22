c8e76aeea2faccc48836aa9c8ee5bcb9fb68dec1
kaorulego5x <kaorulego5x@gmail.com>
2022-11-22 16:16:24 +0800

    styled writebody

--- src/components/screens/WriteBody/hooks.ts
+++ src/components/screens/WriteBody/hooks.ts
@@ -1,6 +1,35 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
+import {
+  NativeSyntheticEvent,
+  TextInputContentSizeChangeEventData,
+} from "react-native";
+
+export const LineHeight = 27;
+
 export const useWriteBody = () => {
   const [title, setTitle] = useState<string>("");
   const [body, setBody] = useState<string>("");
-  return { title, body, setTitle, setBody };
+
+  const [numberOfLines, setNumberOfLines] = useState<number>(0);
+  const displayNumberOfLines = useMemo(() => {
+    return Math.max(numberOfLines, 12);
+  }, [numberOfLines]);
+
+  const onBodyHeightChange = useCallback(
+    (e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>) => {
+      setNumberOfLines(
+        Math.round(e.nativeEvent.contentSize.height / LineHeight)
+      );
+    },
+    []
+  );
+
+  return {
+    title,
+    body,
+    displayNumberOfLines,
+    setTitle,
+    setBody,
+    onBodyHeightChange,
+  };
 };
