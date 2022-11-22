e5ba03c20773610885a75787d6b1ab049b168506
numa123 <taka0201ttt@gmail.com>
2022-11-22 22:21:43 +0900

    add initial state and useEffect

---
+++ src/components/screens/PostDetail/index.ts
@@ -0,0 +1,26 @@
+import { Box } from "native-base";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Post } from "../../../domain/types/Post";
+
+export const PostDetailScreen: React.FC = () => {
+  const [postData, setPostData] = useState<Post>({
+    authorId: "",
+    title: "",
+    body: "",
+    likes: [],
+    hashTags: [],
+    createdAt: new Date(), //Dateの初期値は何にすべきなのか
+    comments: [],
+  });
+
+  useEffect(() => {
+    //データを取ってくる関数 型注釈てきとーです
+    const fetchData = async (): Promise<Post | any> => {};
+
+    const postData = await fetchData();
+    setPostData(postData);
+  }, []);
+
+  return <></>;
+};
